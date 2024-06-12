a7ee0a2d597a819aaf2fa89f6d61e93589194733
josheyr <joshey.raine@outlook.com>
2024-06-12 12:15:24 +0100

    Update mods.go

--- pkg/datatables/mods.go
+++ pkg/datatables/mods.go
@@ -4,44 +4,42 @@ import (
 	"fmt"
 	"github.com/josheyr/go-mongo-datatables/pkg/util"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"strconv"
 )
 
-func DateModFunc(item interface{}, row map[string]interface{}) interface{} {
-	// assert that item to time.Time
-	unixTime, ok := item.(primitive.DateTime)
-	if !ok {
-		// if it's not a primitive.DateTime, it might be a int64
-		unixTimeInt64, ok := item.(int64)
+func DateModFunc(format string) func(item interface{}, row map[string]interface{}) interface{} {
+	return func(item interface{}, row map[string]interface{}) interface{} {
+		// assert that item to time.Time
+		unixTime, ok := item.(primitive.DateTime)
 		if !ok {
-			fmt.Println("item is not a time? we accept DateTime and int64")
-			return item
+			// if it's not a primitive.DateTime, it might be a int64
+			unixTimeInt64, ok := item.(int64)
+			if !ok {
+				fmt.Println("item is not a time? we accept DateTime and int64")
+				return item
+			}
+
+			unixTime = primitive.DateTime(unixTimeInt64 * 1000)
 		}
 
-		unixTime = primitive.DateTime(unixTimeInt64 * 1000)
-	}
+		tm := unixTime.Time()
 
-	tm := unixTime.Time()
+		if tm.IsZero() {
+			return struct {
+				Unix      int64
+				Formatted string
+			}{
+				0, "N/A",
+			}
+		}
 
-	if tm.IsZero() {
 		return struct {
 			Unix      int64
 			Formatted string
 		}{
-			0, "N/A",
+			tm.Unix(),
+			tm.Format(format),
 		}
 	}
-
-	return struct {
-		Unix      int64
-		Formatted string
-	}{
-		tm.Unix(),
-		tm.Format("01/02/2006 15:04:05"),
-	}
-
-	// format date with nice date format with time
-	return "<span style=\"display: none;\">" + strconv.Itoa(int(tm.Unix())) + "</span>" + tm.Format("2006-01-02 15:04:05")
 }
 
 func DurationModFunc(item interface{}, row map[string]interface{}) interface{} {
